8c6087b2c631b664a65450242292aeb4eb0b7a87
kqarlos <kqarlos@hotmail.com>
2019-12-13 13:14:16 -0800

    Adds 5 day forecast cards to display

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,6 +19,7 @@ $("#searchLocation").on("click", function () {
     $("#locationInput").val("");
 
     query(location);
+    queryForecast(location)
 
     addButton(location);
 
@@ -26,10 +27,33 @@ $("#searchLocation").on("click", function () {
 });
 
 $(document).on("click", ".city-button", function () {
-    console.log("clicked");
     var location = $(this).attr("data-city");
     query(location);
+    queryForecast(location);
 });
+
+function queryForecast(location) {
+
+    //query building...
+    var APIKey = "e42ce6fff3cc019aac43965299686295";
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + location + "&units=imperial&cnt=5&appid=" + APIKey;
+    http://api.openweathermap.org/data/2.5/forecast?q=London&appid=e42ce6fff3cc019aac43965299686295
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(queryURL);
+        console.log(response);
+        var forecast = response.list;
+        for (var i = 0; i < forecast.length; i++) {
+            var date = forecast[i].dt_txt;
+            var temperature = forecast[i].main.temp;
+            var humidity = forecast[i].main.humidity;
+            addCard(i, date, temperature, humidity);
+        }
+    });
+}
 
 function query(location) {
     //query building
@@ -40,8 +64,8 @@ function query(location) {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(queryURL);
-        console.log(response);
+        // console.log(queryURL);
+        // console.log(response);
         var lat = response.coord.lat;
         var lon = response.coord.lon;
 
@@ -53,14 +77,50 @@ function query(location) {
             url: queryURL,
             method: "GET"
         }).then(function (uvresponse) {
-            console.log(queryURL);
-            console.log(uvresponse);
+            // console.log(queryURL);
+            // console.log(uvresponse);
             renderCurrentWeather(response.name, response.main.temp, response.main.humidity, response.wind.speed, uvresponse.value);
         });
 
 
     });
 
+}
+
+function addCard(index, date, temperature, humidity) {
+
+    console.log("creating card #: " + index);
+
+    var card = $("<div>");
+    card.addClass("card");
+
+    var cardBody = $("<div>");
+    cardBody.addClass("card-body");
+
+    var title = $("<h5>");
+    title.addClass("card-title");
+    title.text(date);
+
+    var t = $("<p>");
+    t.addClass("card-text");
+    t.text(temperature);
+
+    var h = $("<p>");
+    h.addClass("card-text");
+    h.text(humidity);
+
+    cardBody.append(title);
+    cardBody.append(t);
+    cardBody.append(h);
+    console.log("comleted card body: ");
+    console.log(cardBody);
+
+    card.append(cardBody);
+    console.log("comleted card: ");
+    console.log(card);
+
+    $("#" + index).empty();
+    $("#" + index).append(card);
 }
 
 function addButton(location) {
