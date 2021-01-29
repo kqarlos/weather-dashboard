ab3e6ca97b5ab1d54e313b097ea7fa7062b0b2a9
kqarlos <kqarlos@hotmail.com>
2021-01-29 04:41:58 -0800

    add uvindex warning

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,7 +23,7 @@ function getIcon(condition) {
     }
 }
 
-//Clear information
+//Clear information and render according to data
 function renderCurrentWeather(location, temperature, humidity, windSpeed, uv, condition) {
     $("#location").empty().append(`${location} `);
     let date = moment().format("MM" + "/" + "DD" + "/" + "YYYY");
@@ -39,19 +39,39 @@ function renderCurrentWeather(location, temperature, humidity, windSpeed, uv, co
 
     $("#windSpeed").empty().append(`${windSpeed} MPH`);
 
-    $("#uv").empty();
-    if (uv < 3)
-        $("#uv").css("background-color", "green");
-    else if (uv < 6)
-        $("#uv").css("background-color", "yellow");
-    else if (uv < 8)
-        $("#uv").css("background-color", "orange");
-    else if (uv < 11)
-        $("#uv").css("background-color", "red");
-    else
-        $("#uv").css("background-color", "purple");
+    let uvWarnings = {
+        green: "You can safely stay outside using standard daily sun protection: broad spectrum SPF 30+ sunscreen containing zinc, sunglasses, and hat. Don't forget: in winter, reflection off snow can nearly double UV strength.",
+        yellow: "Stay in the shade during late morning through mid-afternoon. Wear broad spectrum SPF 30+ sunscreen containing zinc, sunglasses, and hat.",
+        orange: "Stay in the shade as much as possible, especially during late morning through mid-afternoon. Wear broad spectrum SPF 30+ sunscreen containing zinc, protective clothing (long-sleeved shirt and pants), sunglasses, and wide-brimmed hat.",
+        red: "Extra protection needed. Be careful outside, especially during late morning through mid-afternoon. Stay in the shade as much as possible, especially during late morning through mid-afternoon. Wear broad spectrum SPF 30+ sunscreen containing zinc, protective clothing (long-sleeved shirt and pants), sunglasses, and wide-brimmed hat. Please note: white sand on the beach will reflect UV rays and can double UV exposure.",
+        purple: "Extra protection needed. Avoid sun exposure during late morning through mid-afternoon. Unprotected skin and eyes can burn in minutes. Wear broad spectrum SPF 30+ sunscreen containing zinc, protective clothing (like long-sleeves), sunglasses, and wide-brimmed hat. Please note: white sand on the beach will reflect UV rays and can double UV exposure."
+    }
 
-    $("#uv").append(uv);
+    let warnings = [];
+    let uvWarning = $("<div>");
+    if (uv < 3) {
+        $("#uv").css("background-color", "green");
+        uvWarning.append(uvWarnings.green);
+    }
+    else if (uv < 6) {
+        $("#uv").css("background-color", "yellow");
+        uvWarning.append(uvWarnings.yellow);
+    }
+    else if (uv < 8) {
+        $("#uv").css("background-color", "orange");
+        uvWarning.append(uvWarnings.orange);
+    }
+    else if (uv < 11) {
+        $("#uv").css("background-color", "red");
+        uvWarning.append(uvWarnings.red);
+    }
+    else {
+        $("#uv").css("background-color", "purple");
+        uvWarning.append(uvWarnings.purple);
+    }
+
+    $("#warnings").empty().append(uvWarning);
+    $("#uv").empty().append(uv);
 }
 
 //On click, handle logic to search for weather given a location
@@ -76,6 +96,7 @@ function formatDate(date) {
     return `${date.getMonth() + 1}/${date.getDate()}/${date.getUTCFullYear()}`;
 }
 
+// Queries for the location weather and calls to render and update data
 function query(location) {
     //query building
     let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=imperial&appid=" + APIKey;
@@ -87,12 +108,11 @@ function query(location) {
                 console.log("1", response);
                 let lat = response.coord.lat;
                 let lon = response.coord.lon;
-                //query building...
+                //query building for uvi and forecats...
                 queryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&units=imperial&appid=${APIKey}`;
-
                 fetch(queryURL).then(data => data.json().then(data => {
                     console.log("2", data);
-
+                    //Call to generate a forecast card for the next five days
                     for (let i = 0; i < 5; i++) {
                         var date = data.daily[i].dt;
                         var temperature = data.daily[i].temp.day;
@@ -100,9 +120,11 @@ function query(location) {
                         var condition = data.daily[i].weather[0].main;
                         addCard(i, date, temperature, humidity, condition);
                     }
+                    //Call to render current weather to main section
                     renderCurrentWeather(response.name, response.main.temp, response.main.humidity, response.wind.speed, data.current.uvi, response.weather[0].main);
                 }));
 
+                // Check if location seaerched is new, if it is, add button, and update locations array and local storage
                 if (!locations.includes(response.name.toUpperCase())) {
                     addButton(response.name.toUpperCase());
                     locations.push(response.name.toUpperCase());
@@ -112,13 +134,16 @@ function query(location) {
             });
 
         } else {
+            // Catch empty string
             alert("Error: " + response.statusText);
         }
     }).catch((error) => {
+        //Catch invalid
         alert(error);
     });
 }
 
+// Renders a forecast card
 function addCard(index, date, temperature, humidity, condition) {
 
     let card = $("<div>");
@@ -155,8 +180,8 @@ function addCard(index, date, temperature, humidity, condition) {
     $("#" + index).empty().append(card);
 }
 
+// Renders a button with the name of a location into the city history container
 function addButton(location) {
-
     let button = $("<button>");
     button.addClass("list-group-item list-group-item-action city-button");
     button.attr("type", "button");
@@ -165,6 +190,7 @@ function addButton(location) {
     $("#history").prepend(button);
 }
 
+// Load locations from local sotrage and render city buttons if needed
 function setUp() {
     locations = JSON.parse(localStorage.getItem("locations")) || []
     for (let i = 0; i < locations.length; i++) {
@@ -172,18 +198,18 @@ function setUp() {
     }
 }
 
+// Save locations array to local storage
 function save() {
     localStorage.setItem("locations", JSON.stringify(locations));
 }
 
+// Clears locations array, calls to udate local storage and removes all city buttons
 function clear() {
     locations = [];
     save();
     $(".city-button").remove();
 }
 
-$("#clear").on("click", function () {
-    clear();
-})
+$("#clear").on("click", clear);
 //set up when ready
 $(setUp()); 
